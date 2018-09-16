075d3fa8509f46d527e1f5b14ebcb315c1da4f48
Lukasz Zywluk <lukasz.zywluk@gmail.com>
2018-09-16 08:33:58 -0400

    Add HTTP interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './/material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { AuthHttpInterceptor } from './auth/auth-http-interceptor'
 import { AuthService } from './auth/auth.service'
-import { HomeComponent } from './home/home.component'
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { LoginComponent } from './login/login.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { SimpleDialogComponent } from './common/simple-dialog/simple-dialog.component'
 import { UiService } from './common/ui.service'
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
@@ -32,7 +33,15 @@ import { UiService } from './common/ui.service'
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [AuthService, UiService],
+  providers: [
+    AuthService,
+    UiService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
