fbdef9cfd1032587ecbc45c43ac526ce3de2839e
Lukasz Zywluk <lukasz.zywluk@gmail.com>
2018-09-16 14:55:58 -0400

    Add MatSidenavModule to user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,9 +5,10 @@ import { UserRoutingModule } from './user-routing.module'
 import { ProfileComponent } from './profile/profile.component'
 import { LogoutComponent } from './logout/logout.component'
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import { MatSidenavModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, UserRoutingModule, MatSidenavModule],
   declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
 })
 export class UserModule {}
